ccc096cc4a389bda92359415757d04651050f909
Lorsmo <51829277+Lorsmo@users.noreply.github.com>
2019-09-18 09:25:26 -0500

    report

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -1,3 +1,58 @@
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS titles;
+DROP TABLE IF EXISTS salaries;
+
+CREATE TABLE employees (
+    emp_no INTEGER PRIMARY KEY  NOT NULL,
+    birth_date DATE   NOT NULL,
+    first_name VARCHAR(50)   NOT NULL,
+    last_name VARCHAR(50)   NOT NULL,
+    gender VARCHAR(5)   NOT NULL,
+    hire_date DATE   NOT NULL
+);
+
+CREATE TABLE departments (
+    dept_no VARCHAR(10) PRIMARY KEY   NOT NULL,
+    dept_name VARCHAR(50)   NOT NULL
+);
+
+CREATE TABLE dept_emp (
+	emp_no INTEGER   NOT NULL,
+    dept_no VARCHAR(10)   NOT NULL,
+    from_date DATE   NOT NULL,
+    to_date DATE   NOT NULL,
+	FOREIGN KEY(emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY(dept_no) REFERENCES departments (dept_no)	
+);
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(10)   NOT NULL,
+    emp_no INTEGER   NOT NULL,
+    from_date DATE   NOT NULL,
+    to_date DATE   NOT NULL,
+	FOREIGN KEY(dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY(emp_no) REFERENCES employees (emp_no)	
+);
+
+CREATE TABLE salaries (
+    emp_no INTEGER   NOT NULL,
+    salary MONEY   NOT NULL,
+    from_date DATE   NOT NULL,
+    to_date DATE   NOT NULL,
+	FOREIGN KEY(emp_no) REFERENCES employees (emp_no)
+);
+
+CREATE TABLE titles (
+    emp_no INTEGER   NOT NULL,
+    title VARCHAR(50)   NOT NULL,
+    from_date DATE   NOT NULL,
+    to_date DATE   NOT NULL,
+	FOREIGN KEY(emp_no) REFERENCES employees (emp_no)
+);
+
 -- List the following details of each employee: employee number, last name, first name, gender, and salary.
 SELECT e.emp_no, e.last_name, e.first_name, e.gender, s.salary
 FROM employees e
@@ -43,7 +98,8 @@ JOIN dept_emp de
 ON e.emp_no = de.emp_no
 JOIN departments d
 ON de.dept_no = d.dept_no
-WHERE d.dept_name = 'Sales';
+WHERE d.dept_name = 'Sales'
+;
 
 -- List all employees in the Sales and Development departments, including their employee number, last name,
 -- first name, and department name.
@@ -54,7 +110,8 @@ ON e.emp_no = de.emp_no
 JOIN departments d
 ON de.dept_no = d.dept_no
 WHERE d.dept_name = 'Sales'
-OR d.dept_name = 'Development';
+OR d.dept_name = 'Development'
+;
 
 -- In descending order, list the frequency count of employee last names, i.e.,
 -- how many employees share each last name.
